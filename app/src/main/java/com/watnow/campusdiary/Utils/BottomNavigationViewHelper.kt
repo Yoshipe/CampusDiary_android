79746e48a12a9f9c7c57c81e860d048792c430a7
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-12 00:41:23 +0900

    Add a new function which control bottomNavViewEx

--- app/src/main/java/com/watnow/campusdiary/Utils/BottomNavigationViewHelper.kt
+++ app/src/main/java/com/watnow/campusdiary/Utils/BottomNavigationViewHelper.kt
@@ -1,5 +1,19 @@
 package com.watnow.campusdiary.Utils
 
+import android.util.Log
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
+
 /**
  * Created by saitoushunsuke on 2018/02/12.
  */
+open class BottomNavigationViewHelper {
+    private val TAG: String = "BottomNavigationViewHelper"
+
+    fun setupBottomNavigationView(bottomNavigationViewEx: BottomNavigationViewEx) {
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView")
+        bottomNavigationViewEx.enableAnimation(false)
+        bottomNavigationViewEx.enableItemShiftingMode(false)
+        bottomNavigationViewEx.enableShiftingMode(false)
+        bottomNavigationViewEx.setTextVisibility(false)
+    }
+}
