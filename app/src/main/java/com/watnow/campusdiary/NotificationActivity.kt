32b1fb24b1a485669946ec318fd50169d3960153
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-12 00:59:23 +0900

    Add three Activities which are MapActivity TimeTableActivity NotificationActivity

---
+++ app/src/main/java/com/watnow/campusdiary/NotificationActivity.kt
@@ -0,0 +1,36 @@
+package com.watnow.campusdiary
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
+import com.watnow.campusdiary.Utils.BottomNavigationViewHelper
+
+/**
+ * Created by saitoushunsuke on 2018/02/12.
+ */
+class NotificationActivity : AppCompatActivity() {
+
+    private val TAG: String = "NotificationActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: starting")
+
+        setupBottomNavigationView()
+    }
+
+    /* *
+ *  BottomNavigationView setup
+ */
+    private fun setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView")
+        val bottomNavigationViewEx: BottomNavigationViewEx = findViewById(R.id.bottomNavViewBar)
+        var bottomNavViewHelper: BottomNavigationViewHelper = BottomNavigationViewHelper()
+        bottomNavViewHelper.setupBottomNavigationView(bottomNavigationViewEx)
+
+    }
+
+}
