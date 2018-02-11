2291a3d8832077e6ed5895283c105869efd826a1
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-12 00:40:33 +0900

    Add a function to enable animation of bottomNavView's icons

--- app/src/main/java/com/watnow/campusdiary/MainActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/MainActivity.kt
@@ -2,11 +2,26 @@ package com.watnow.campusdiary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
+import com.watnow.campusdiary.Utils.BottomNavigationViewHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    /* *
+     *  BottomNavigationView setup
+     */
+    private fun setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView")
+        val bottomNavigationViewEx: BottomNavigationViewEx = findViewById(R.id.bottomNavViewBar)
+        var bottomNavViewHelper: BottomNavigationViewHelper = BottomNavigationViewHelper()
+        bottomNavViewHelper.setupBottomNavigationView(bottomNavigationViewEx)
+
     }
 }
