1636034ef2e37fb6e06ce2c149d426c56ad73ea9
Yoshipe <sho318go@gmail.com>
2018-02-14 23:53:47 +0900

    :lipstick:　無限スクロールカレンダーに1-31が連続で続くページを作成した

--- app/src/main/java/com/watnow/campusdiary/Calendar/CalendarActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/Calendar/CalendarActivity.kt
@@ -3,6 +3,8 @@ package com.watnow.campusdiary.Calendar
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -27,9 +29,18 @@ class CalendarActivity : AppCompatActivity(), CalendarViewHolder.ItemClickListen
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar_activity)
         setupBottomNavigationView()
-        val hoges:List<String> = listOf("1","2")
-        calendarRecycleView.adapter = CalendarRecycleAdapter(this, this, hoges)
-        calendarRecycleView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        var hoges:List<String>? = null
+        val tmpList: MutableList<String> = mutableListOf()
+        val year: Int = 5
+        for (i in 1..12*year) {
+            for (j in 1..31) {
+                tmpList.add(j.toString())
+            }
+        }
+        hoges = tmpList
+        calendarRecycleView.adapter = CalendarRecycleAdapter(this, this, hoges?: listOf("error"))
+        calendarRecycleView.layoutManager = GridLayoutManager(this,7)
+
 
     }
 
