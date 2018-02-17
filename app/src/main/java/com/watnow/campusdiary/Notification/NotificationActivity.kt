7ca67535fbc4abfc32c6dc8ce000a6f8067f2c72
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-17 21:02:23 +0900

    :sparkles:NotificationActivityのListViewにClickListenerを追加、
    NotificationImageActivityに遷移する関数を作成

--- app/src/main/java/com/watnow/campusdiary/Notification/NotificationActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/Notification/NotificationActivity.kt
@@ -1,11 +1,13 @@
 package com.watnow.campusdiary.Notification
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.*
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 import com.watnow.campusdiary.R
@@ -22,25 +24,33 @@ class NotificationActivity : AppCompatActivity() {
 
     private val mContext: Context = this
 
+    // parameters
+    private lateinit var notificationList: ListView
+    private lateinit var searchTxt: TextView
+    private lateinit var searchButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.notification_activity)
         Log.d(TAG, "onCreate: starting")
 
+        // initializing all parameters
+        this.notificationList = findViewById(R.id.notifications_container)
+        this.searchTxt = findViewById(R.id.search_txt)
+        this.searchButton = findViewById(R.id.search_button)
+
+        // setup BottomNavigationView
         setupBottomNavigationView()
     }
 
     override fun onStart() {
         super.onStart()
-        val searchButton: Button = findViewById(R.id.search_button)
-        val searchTxt: EditText = findViewById(R.id.search_txt)
         searchButton.setOnClickListener {
             Toast.makeText(this, searchTxt.text, Toast.LENGTH_SHORT).show()
         }
         // ListViewを設定
-        val notificationList: ListView = findViewById(R.id.notifications_container)
-        val listMaker: NotificationListViewAdapter = NotificationListViewAdapter(mContext, notificationList)
-
+        NotificationListViewAdapter(mContext, notificationList)
+        leadImageFromListClicked(notificationList)
     }
 
     /* *
@@ -55,6 +65,17 @@ class NotificationActivity : AppCompatActivity() {
         val menu: Menu = bottomNavigationViewEx.menu
         val menuItem: MenuItem = menu.getItem(ACTIVITY_NUM)
         menuItem.isChecked = true
+    }
+
+    /**
+     * ListView StartActivity
+     */
+    private fun leadImageFromListClicked(listView: ListView) {
+        val intent: Intent = Intent(mContext, NotificationImageActivity::class.java)
+        listView.setOnItemClickListener { parent, view, position, id ->
+            intent.putExtra("position", position)
+            startActivity(intent)
+        }
     }
 
 }
