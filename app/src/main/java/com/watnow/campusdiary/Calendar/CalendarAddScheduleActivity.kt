d1ec8c1ecc9dabbf93296b645f49b83c51abfd74
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-26 22:55:46 +0900

    :+1:カレンダーで予定を追加するアクティビティを新規作成

---
+++ app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.watnow.campusdiary.Calendar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarAddScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_add_schedule)
+    }
+}
