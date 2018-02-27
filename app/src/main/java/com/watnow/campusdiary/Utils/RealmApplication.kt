3fe39da05fee8189ffbfcb99a5ff2ea2bce12538
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-28 01:04:34 +0900

    :sparkles:onCreateメソッド内でRealmを初期化

--- app/src/main/java/com/watnow/campusdiary/Utils/RealmApplication.kt
+++ app/src/main/java/com/watnow/campusdiary/Utils/RealmApplication.kt
@@ -1,7 +1,17 @@
 package com.watnow.campusdiary.Utils
 
+import android.app.Application
+import io.realm.Realm
+
 /**
  * Created by saitoushunsuke on 2018/02/28.
  */
-class RealmApplication {
+class RealmApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Realmの初期化
+        Realm.init(this@RealmApplication)
+    }
 }
