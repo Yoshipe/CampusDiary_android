ccea65e8bd260548373a82d3eca470a7424ddb43
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-02-28 01:44:57 +0900

    :sparkles:時間割のアクティビティで新規登録に対して、データベースに保存するところまで完了

--- app/src/main/java/com/watnow/campusdiary/TimeTable/TimeTableActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/TimeTable/TimeTableActivity.kt
@@ -13,6 +13,7 @@ import android.widget.*
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 import com.watnow.campusdiary.R
 import com.watnow.campusdiary.Utils.BottomNavigationViewHelper
+import io.realm.Realm
 import kotlinx.android.synthetic.main.layout_time_table_contents.*
 
 /**
@@ -24,7 +25,9 @@ class TimeTableActivity : AppCompatActivity(), View.OnClickListener {
 
     private val ACTIVITY_NUM: Int = 2
 
-    private val mContext: Context = this
+    private val mContext: Context = this@TimeTableActivity
+
+    lateinit var realm: Realm
 
     // 画面立ち上げ時に１回だけ実行する
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +78,16 @@ class TimeTableActivity : AppCompatActivity(), View.OnClickListener {
     override fun onResume() {
         super.onResume()
         Log.d("onResume", ": Started")
+        // Realmのインスタンス取得
+        realm = Realm.getDefaultInstance()
+
+        // DBより時間割を作成
+        setButtonText()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        realm.close()
     }
 
     /* *
@@ -92,55 +105,92 @@ class TimeTableActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
+        // 受け取ったビューをボタンにキャスト
+        val button: Button = view as Button
+        if (button.text.toString() == "") {
+            addSubject(button)
+        } else {
+            changeSubject()
+        }
+    }
+
+
+    private fun changeSubject() {
+        Toast.makeText(this@TimeTableActivity, "changeSubjec is called", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun addSubject(button: Button) {
         val customLayout = layoutInflater.inflate(R.layout.component_time_table_dialog, null)
+
+        // inflateしたレイアウトから各ビューを紐付け
         val subject: EditText = customLayout.findViewById(R.id.subjectName)
         val classRoom: EditText = customLayout.findViewById(R.id.classRoom)
-        AlertDialog.Builder(mContext).apply {
+
+        // AlertDialogを生成する
+        AlertDialog.Builder(this@TimeTableActivity).apply {
             setView(customLayout)
             setTitle("時間割登録")
-            setPositiveButton("登録", DialogInterface.OnClickListener { dialogInterface, which ->
-                // 登録ボタンを押した時の処理
-                val content: String = subject.text.toString() + "\n" + classRoom.text.toString()
-                when (view!!.id) {
-                    button1_1.id -> button1_1.text = content
-                    button1_2.id -> button1_2.text = content
-                    button1_3.id -> button1_3.text = content
-                    button1_4.id -> button1_4.text = content
-                    button1_5.id -> button1_5.text = content
-                    button1_6.id -> button1_6.text = content
-                    button1_7.id -> button1_7.text = content
-                    button2_1.id -> button2_1.text = content
-                    button2_2.id -> button2_2.text = content
-                    button2_3.id -> button2_3.text = content
-                    button2_4.id -> button2_4.text = content
-                    button2_5.id -> button2_5.text = content
-                    button2_6.id -> button2_6.text = content
-                    button2_7.id -> button2_7.text = content
-                    button3_1.id -> button3_1.text = content
-                    button3_2.id -> button3_2.text = content
-                    button3_3.id -> button3_3.text = content
-                    button3_4.id -> button3_4.text = content
-                    button3_5.id -> button3_5.text = content
-                    button3_6.id -> button3_6.text = content
-                    button3_7.id -> button3_7.text = content
-                    button4_1.id -> button4_1.text = content
-                    button4_2.id -> button4_2.text = content
-                    button4_3.id -> button4_3.text = content
-                    button4_4.id -> button4_4.text = content
-                    button4_5.id -> button4_5.text = content
-                    button4_6.id -> button4_6.text = content
-                    button4_7.id -> button4_7.text = content
-                    button5_1.id -> button5_1.text = content
-                    button5_2.id -> button5_2.text = content
-                    button5_3.id -> button5_3.text = content
-                    button5_4.id -> button5_4.text = content
-                    button5_5.id -> button5_5.text = content
-                    button5_6.id -> button5_6.text = content
-                    button5_7.id -> button5_7.text = content
-                }
+            setPositiveButton("登録", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(this@TimeTableActivity, "登録", Toast.LENGTH_SHORT).show()
+                // 登録ボタンを押したときのDBへの登録
+                // realm開始
+                realm.beginTransaction()
+                val timeTableDB = realm.createObject(TimeTableDB::class.java)
+                timeTableDB.strId = getString(button.id)
+                timeTableDB.strSubject = subject.text.toString()
+                timeTableDB.strClassRoom = classRoom.text.toString()
+                // realm終了
+                realm.commitTransaction()
+                // 該当するボタンのテキストに登録
+                setButtonText()
             })
             setNegativeButton("取り消し", null)
             show()
+        }
+    }
+
+    private fun setButtonText() {
+        val result = realm.where(TimeTableDB::class.java).findAll().sort("strId")
+        result.forEach {
+            // 全てのデータをみて、合致する場所にテキストをセットする
+            when (it.strId) {
+                getString(button1_1.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_2.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_3.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_4.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_5.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_6.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button1_7.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_1.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_2.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_3.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_4.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_5.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_6.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button2_7.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_1.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_2.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_3.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_4.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_5.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_6.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button3_7.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_1.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_2.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_3.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_4.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_5.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_6.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button4_7.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_1.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_2.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_3.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_4.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_5.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_6.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                getString(button5_7.id) -> button1_1.text = it.strSubject + "\n" + it.strClassRoom
+                else -> Toast.makeText(this@TimeTableActivity, "ERROR REGISTERING", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
