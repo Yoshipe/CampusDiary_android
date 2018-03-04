450b1176331007f9bb1fa027d1329929abd7769b
Yoshipe <sho318go@gmail.com>
2018-03-04 19:32:12 +0900

    :sparkles: 予定作成時のアクションバーのクリックイベントを追加

--- app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
@@ -1,7 +1,10 @@
 package com.watnow.campusdiary.Calendar
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import com.watnow.campusdiary.R
 import kotlinx.android.synthetic.main.activity_calendar_add_schedule.*
 
@@ -10,6 +13,20 @@ class CalendarAddScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_add_schedule)
-        // Todo 1. 各ボタンのクリック処理を書く
+        new_event_theme.setOnClickListener { object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+            }
+        }}
+        new_event_cancel_btn.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                finish()
+            }
+        })
+        new_event_save_btn.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@CalendarAddScheduleActivity, CalendarActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
