de9d899fef79f5ff0e69ae4130e6a2c595163103
Yoshipe <sho318go@gmail.com>
2018-03-05 02:14:16 +0900

    :bug: clickListenerが聞かなかったので修正

--- app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
@@ -3,6 +3,7 @@ package com.watnow.campusdiary.Calendar
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import com.watnow.campusdiary.R
@@ -13,10 +14,10 @@ class CalendarAddScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_add_schedule)
-        new_event_theme.setOnClickListener { object : View.OnClickListener{
+        new_event_theme.setOnClickListener ( object : View.OnClickListener{
             override fun onClick(p0: View?) {
             }
-        }}
+        })
         new_event_cancel_btn.setOnClickListener(object: View.OnClickListener{
             override fun onClick(p0: View?) {
                 finish()
