c90e40e1d8e8899f5e95d3b83fa23165a27b7a07
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-03-09 16:13:35 +0900

    :sparkles:NavigationDrawerの画面遷移のための準備

---
+++ app/src/main/java/com/watnow/campusdiary/RegulationActivity.kt
@@ -0,0 +1,12 @@
+package com.watnow.campusdiary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegulationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regulation)
+    }
+}
---
+++ app/src/main/java/com/watnow/campusdiary/SchoolYearActivity.kt
@@ -0,0 +1,12 @@
+package com.watnow.campusdiary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SchoolYearActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_school_year)
+    }
+}
