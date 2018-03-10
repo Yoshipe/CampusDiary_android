7036e9c52c86748ed07b2bec283700acbd670717
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-03-10 21:07:20 +0900

    :sparkles:Realmデータベースに登録ができるようになった

--- app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
+++ app/src/main/java/com/watnow/campusdiary/Calendar/CalendarAddScheduleActivity.kt
@@ -4,24 +4,35 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.View
 import android.widget.*
 import com.watnow.campusdiary.R
+import com.watnow.campusdiary.RealmDB.CalendarDB
+import com.watnow.campusdiary.Utils.Constant
+import io.realm.Realm
+import io.realm.RealmObject
 import kotlinx.android.synthetic.main.activity_calendar_add_schedule.*
+import kotlinx.android.synthetic.main.layout_calendar_add_alert_dialog.*
 
 class CalendarAddScheduleActivity : AppCompatActivity() {
 
     var colorId: Int = R.color.ruby
 
+    lateinit var realm: Realm
+    private var date: String = ""
+    private lateinit var inflatedView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_add_schedule)
+        date = intent.extras.getString(Constant.INTENT_KEY_DATE.name)
         new_event_theme.setOnClickListener {
-            val view = layoutInflater.inflate(R.layout.layout_calendar_add_alert_dialog, null)
-            val radioGroup = view.findViewById<RadioGroup>(R.id.radioGroup)
+            inflatedView = layoutInflater.inflate(R.layout.layout_calendar_add_alert_dialog, null)
+            val radioGroup = inflatedView.findViewById<RadioGroup>(R.id.radioGroup)
             val dialog = AlertDialog.Builder(this@CalendarAddScheduleActivity).apply {
-                setView(view)
+                setView(inflatedView)
                 setPositiveButton("OK") {dialogInterface, i ->
-                    val selectedButton = view.findViewById<RadioButton>(radioGroup.checkedRadioButtonId)
+                    val selectedButton = inflatedView.findViewById<RadioButton>(radioGroup.checkedRadioButtonId)
                     colorId = getSelectedColor(selectedButton)
                     buttonColorSelect.setBackgroundResource(colorId)
                     new_event_navigation.setBackgroundResource(colorId)
@@ -34,10 +45,27 @@ class CalendarAddScheduleActivity : AppCompatActivity() {
             finish()
         }
         new_event_save_btn.setOnClickListener {
-            // Todo Realmに保存
+            realm.beginTransaction()
+            val targetDB = realm.createObject(CalendarDB::class.java)
+            targetDB.title = new_event_title.text.toString()
+            val radioGroup = inflatedView.findViewById<RadioGroup>(R.id.radioGroup)
+            targetDB.theme = inflatedView.findViewById<RadioButton>(radioGroup.checkedRadioButtonId).text.toString()
+            targetDB.detail = new_event_description.text.toString()
+            targetDB.date = date
+            realm.commitTransaction()
             finish()
             Toast.makeText(this@CalendarAddScheduleActivity, "登録しました", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        realm = Realm.getDefaultInstance()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        realm.close()
     }
 
     private fun getSelectedColor(selectedButton: RadioButton): Int {
